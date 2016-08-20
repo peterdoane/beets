2223589ebf9e330289244fa6ac0b22ead22ce2a2
Hien Dang <hienpd@outlook.com>
2016-08-20 16:52:15 -0700

    Migrations

---
+++ migrations/20160820160609_beets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('beets', (table) => {
+    table.increments();
+    table.string('name').notNullable().defaultTo('');
+    table.string('image_url').notNullable().defaultTo('');
+    table.string('sequence').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('beets');
+};
