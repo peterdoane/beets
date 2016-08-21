1b97b907100d0d49e142102e0fee908116d84e73
Peter Doane <peterdoane@gmail.com>
2016-08-21 00:04:54 -0700

    I've got two versions!

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = React.createClass({
   render() {
     const style = {
-      background: 'yellow',
+      background: '#F5A623',
       boxSizing: 'border-box',
       height: '200px',
     };
     return (
       <div id="header" style={style}>
-          <h1>Test</h1>
+          <h1>Musical Chairs</h1>
       </div>
     );
   }
