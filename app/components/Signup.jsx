6a8e4fa6b10758903d0d89fb2e49038f66d0f79c
Hien Dang <hienpd@outlook.com>
2016-08-22 11:51:41 -0700

    Build out signup form

--- app/components/Signup.jsx
+++ app/components/Signup.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Signup = React.createClass({
   render() {
-    return <h2>Sign Up Now</h2>;
+    return <div>
+      <h2>Sign Up Now</h2>
+      <div>
+        <input type="text" placeholder="Choose username" />
+      </div>
+      <div>
+        <input type="password" placeholder="Choose password" />
+      </div>
+      <div>
+        <button value="signup-btn">Register</button>
+      </div>
+      <Link to={'/'}>Nevermind</Link>
+    </div>;
   }
 });
 
