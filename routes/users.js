4b7dd4b98ff4817d78bff662cb09eea3eb3d4c34
Hien Dang <hienpd@outlook.com>
2016-08-22 15:39:53 -0700

    Change bcrypt to bcrypt-as-promised

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-as-promised');
 const knex = require('../knex');
 // const ev = require('express-validation');
 // const validations = require('../validations/users');
@@ -30,24 +30,17 @@ router.post('/users', (req, res, next) => {
       if (users.length > 0) {
         return res.status(400).set('Content-Type', 'text/plain').send('Username already exists');
       }
-      bcrypt.hash(req.body.password, 12, (hashErr, hashed_password) => {
-        console.log('doing something');
-        if (hashErr) {
-          return next(hashErr);
-        }
-
-        knex('users')
+      return bcrypt.hash(req.body.password, 12);
+    })
+    .then((hashed_password) => {
+      return knex('users')
         .insert({
           username: req.body.username,
           hashed_password: hashed_password
-        })
-        .then((users) => {
-          res.sendStatus(200);
-        })
-        .catch((err) => {
-          next(err);
         });
-      });
+      })
+    .then((users) => {
+      res.sendStatus(200);
     })
     .catch((err) => {
       next(err);
