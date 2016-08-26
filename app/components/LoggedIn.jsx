88538e033c4e143690915e0bc905b16015842e67
Hien Dang <hienpd@outlook.com>
2016-08-26 08:50:38 -0700

    Your beets shows up

--- app/components/LoggedIn.jsx
+++ app/components/LoggedIn.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import axios from 'axios';
 import { notify } from 'react-notify-toast';
 import { withRouter } from 'react-router';
+import cookie from 'react-cookie';
 
 const LoggedIn = React.createClass({
+  getInitialState() {
+    return {
+      username: cookie.load('mc_username'),
+      beets: []
+    };
+  },
+
+  componentWillMount() {
+    let arr = [];
+
+    axios.get(`/api/beets/${this.state.username}`)
+    .then((res) => {
+      for (let i = 0; i < res.data.length; i++) {
+        arr.push(res.data[i].title);
+      }
+
+      this.setState({ beets: arr });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  },
+
   handleLogOut() {
     axios.delete('/api/token')
       .then(() => {
@@ -20,8 +44,15 @@ const LoggedIn = React.createClass({
     return <div className="card logged-in">
       <div className="card-content">
         <span className="card-title">Welcome {this.props.username}!</span>
-        <h4>Your Songs</h4>
-        <button onClick={this.handleLogOut}>Log Out</button>
+        <h5>Your Beets</h5>
+        <ul>
+          {this.state.beets.map((beet, index) => {
+            return <li key={index}>{beet}</li>
+          })}
+        </ul>
+        <div>
+          <button onClick={this.handleLogOut}>Log Out</button>
+        </div>
       </div>
     </div>;
   }
--- routes/beets.js
+++ routes/beets.js
@@ -18,6 +18,23 @@ router.get('/beets', (_req, res, next) => {
     });
 });
 
+// select title, image_url from beets_users inner join users on (users.id = beets_users.user_id) inner join beets on (beets.id = beets_users.beet_id) where username = 'mcfresh';
+
+router.get('/beets/:username', (req, res, next) => {
+  console.log('helloooooooo');
+  knex('beets_users')
+    .select('title')
+    .join('users', 'users.id', '=', 'beets_users.user_id')
+    .join('beets', 'beets.id', '=', 'beets_users.beet_id')
+    .where('username', req.params.username)
+    .then((beets) => {
+      res.send(beets);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 router.post('/beets', checkAuth, (req, res, next) => {
   knex('beets')
     .insert(req.body, '*')
