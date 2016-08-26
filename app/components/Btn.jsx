acc7ded435ae4a18bb66307642e2f483b0eefb11
Michael Park <thatmichaelpark@gmail.com>
2016-08-25 20:04:59 -0700

    linted

--- app/components/Btn.jsx
+++ app/components/Btn.jsx
@@ -2,55 +2,67 @@ import React from 'react';
 
 const Btn = React.createClass({
   getInitialState() {
-    return {isBtnActive: false};
+    return { isBtnActive: false };
   },
   handleClick() {
     this.props.btnClicked(this.props.pattern, this.props.row, this.props.step);
-    this.setState({isBtnActive:!this.state.isBtnActive});
-    // console.log(this.state.isBtnActive);
-    // console.log('step value:', this.props.step, 'activeStep: ', this.props.activeStep);
-    // console.log('pattern: ', this.props.pattern, 'activePattern: ', this.props.activePattern);
+    this.setState({ isBtnActive: !this.state.isBtnActive });
   },
   isActive() {
-    return this.props.step === this.props.activeStep && this.props.pattern === this.props.activePattern;
+    return this.props.step === this.props.activeStep &&
+      this.props.pattern === this.props.activePattern;
   },
   render() {
     const {
       step,
       btnState
     } = this.props;
-    const height = 40;
-    const width = 40;
     const active = this.isActive();
     const buttonClassName = () => {
       if (step <= 3) {
-        return `drum-button-orange ${btnState ? 'pressed' : ''} ${active ? 'active' : ''}`;
-      } else if (step <= 7) {
-        return `drum-button-yellow ${btnState ? 'pressed' : ''} ${active ? 'active' : ''}`;
-      } else if (step <= 11) {
-        return `drum-button-white ${btnState ? 'pressed' : ''} ${active ? 'active' : ''}`;
-      } else {
-        return `drum-button-red ${btnState ? 'pressed' : ''} ${active ? 'active' : ''}`;
+        return `drum-button-orange ${
+          btnState ? 'pressed' : ''
+        } ${active ? 'active' : ''}`;
       }
+      else if (step <= 7) {
+        return `drum-button-yellow ${
+          btnState ? 'pressed' : ''
+        } ${active ? 'active' : ''}`;
+      }
+      else if (step <= 11) {
+        return `drum-button-white ${
+          btnState ? 'pressed' : ''
+        } ${active ? 'active' : ''}`;
+      }
+
+      return `drum-button-red ${
+        btnState ? 'pressed' : ''
+      } ${active ? 'active' : ''}`;
     };
     const style = {
-      // background: this.props.btnState ? (active ? 'green': 'black') : 'white',
-      // border: active ? '1px solid red' : '1px solid black',
-      // boxSizing: 'unset',
-      // height: '5%',
-      // width: '5%',
-      // margin:'.125%'
 
-      // left: `${this.props.step * width}px`,
-      // top: `${this.props.row * height}px`,
+    // background: this.props.btnState ? (active ? 'green': 'black') : 'white',
+    // border: active ? '1px solid red' : '1px solid black',
+    // boxSizing: 'unset',
+    // height: '5%',
+    // width: '5%',
+    // margin:'.125%'
+
+    // left: `${this.props.step * width}px`,
+    // top: `${this.props.row * height}px`,
     };
     const fill = this.state.isBtnActive ? '#fff' : '#000';
+
+/* eslint-disable */
+// eslint does not like the SVG below...
+
     return (
       <div
+        className={buttonClassName()}
         onClick={this.handleClick}
         style={style}
-        className={buttonClassName()}>
-        <svg width="15px" height="26px" viewBox="484 417 15 26">
+      >
+        <svg height="26px" width="15px" viewBox="484 417 15 26">
           <defs>
               <rect id="path-1" x="0" y="0" width="13" height="26"></rect>
               <mask id="mask-2" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox" x="0" y="0" width="13" height="26" fill="white">
