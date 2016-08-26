78f7c3131d7dd670c526409b24ad10f8f270d255
Michael Park <thatmichaelpark@gmail.com>
2016-08-25 19:55:14 -0700

    linted

--- app/components/Beets.jsx
+++ app/components/Beets.jsx
@@ -15,9 +15,10 @@ const Beets = React.createClass({
     .then((res) => {
       beets = res.data;
 
-      const promises = beets.map((beet, index) => {
+      const promises = beets.map((beet) => {
         return axios.get(`/api/beets_users/beet_id/${beet.id}`);
       });
+
       return axios.all(promises);
     })
     .then((res) => {
@@ -28,7 +29,7 @@ const Beets = React.createClass({
       this.setState({ beets });
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err); // eslint-disable-line no-console
     });
   },
 
@@ -38,30 +39,32 @@ const Beets = React.createClass({
     return <div className="card beets">
       <div className="card-content">
         <span className="card-title">Recent Beets</span>
-          <div className="row">
+        <div className="row">
 
-              {beets.map((beet, index) => {
-                return (
-                  <div key={index} className="card horizontal">
-                    <div className="card-image">
-                      <img className="album" src={beet.image_url} />
-                    </div>
-                    <div className="card-stacked">
-                      <div className="card-content">
-                        <span className="card-title">{beet.title}</span>
-                        <p>by {beet.collaborators.map((collab) => {
-                          return collab.username;
-                        }).join(', ')} </p>
+          {beets.map((beet, index) => {
+            return (
+              <div className="card horizontal" key={index}>
+                <div className="card-image">
+                  <img className="album" src={beet.image_url} />
+                </div>
+                <div className="card-stacked">
+                  <div className="card-content">
+                    <span className="card-title">{beet.title}</span>
+                    <p>by {beet.collaborators.map((collab) => {
+                      return collab.username;
+                    }).join(', ')} </p>
 
-                      </div>
-                      <a className="waves-effect waves-light btn"><i className="material-icons">play_arrow / pause</i></a>
-                    </div>
                   </div>
-                );
-              })}
-          </div>
+                  <a className="waves-effect waves-light btn">
+                    <i className="material-icons">play_arrow / pause</i>
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </div>;
   }
 });
 
