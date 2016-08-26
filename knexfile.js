1203419a7225b4cf4accaff9cb986024206888e3
Peter Doane <peterdoane@gmail.com>
2016-08-25 17:00:28 -0700

    prepare for heroku

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/musical_chairs_dev'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
