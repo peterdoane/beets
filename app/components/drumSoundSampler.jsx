35cd56bf0e49e34cbdebd0caa5169a50e08fed0c
Michael Park <thatmichaelpark@gmail.com>
2016-08-25 20:26:46 -0700

    removed drumsoundsampler.jsx

--- app/components/drumSoundSampler.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import DrumMachine from 'components/DrumMachine';
-
-
-const drumSoundSampler = React.createClass({
-  handleClick() {
-    this.props.btnClicked(drums.trigger);
-  },
-});
